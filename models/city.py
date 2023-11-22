6ff0a1220490942699596144cfebe42a7bb194c0
Rigiih7 <mwirigihbrian@gmail.com>
2023-11-22 13:51:47 -0800

    console test

--- models/city.py
+++ models/city.py
@@ -18,5 +18,8 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey("states.id", ondelete='CASCADE'),
                       nullable=False)
 
-    places = relationship("Place", backref="cities")
+    places = relationship(
+        'models.place.Place', 
+        backref='cities',
+        cascade='all, delete-orphan')
 
